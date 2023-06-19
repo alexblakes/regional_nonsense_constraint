83bf377964c771ccf222f35ea8f7c8e6847fd4fd
alex <alex.blakes@gmail.com>
2023-06-19 14:35:27 +0100

    Refine pext liftover script

--- scripts/pext_liftover.sh
+++ scripts/pext_liftover.sh
@@ -1,4 +1,4 @@
 # Liftover the pext annotations from hg19 to hg38
 # Run this script within the "ucsc" conda environment
 
-liftOver ../outputs/pext_37.bed ../data/hg19ToHg38.over.chain.gz ../outputs/pext_38.bed ../outputs/pext_liftover_unmapped.txt
+liftOver -bedPlus=5 -tab ../outputs/pext_37.bed ../data/hg19ToHg38.over.chain.gz ../outputs/pext_38.bed ../outputs/pext_liftover_unmapped.txt
