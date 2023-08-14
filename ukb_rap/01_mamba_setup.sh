35457b82c2b11666f8c47645474a3f6492ed0cd8
Alex <alex.blakes@gmail.com>
2023-08-14 11:59:51 +0000

    Add scripts to initialise UKB RAP worker environment

---
+++ ukb_rap/01_mamba_setup.sh
@@ -0,0 +1,5 @@
+# Install Mamba
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
+bash Mambaforge-Linux-x86_64.sh
+
+# Manual review of licence terms and install location required.
