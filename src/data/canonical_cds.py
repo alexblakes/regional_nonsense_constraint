3294279513847adba0beb391230b9dadaab27107
alex <alex.blakes@gmail.com>
2023-10-04 16:03:29 +0100

    Document the script

--- src/data/canonical_cds.py
+++ src/data/canonical_cds.py
@@ -1,8 +1,25 @@
 """Canonical CDS
 
-This script defines the coordinates of the CDS for canonical transcript in a GENCODE
-annotation. It takes a GENCODE .gtf file as input. It gives a .bed output for each of 
+This script defines the coordinates of the CDS for canonical transcripts in a GENCODE
+annotation. It takes a GENCODE .gtf file as input. It writes a .bed output for each of 
 the desired features.
+
+Functions:
+- get_gencode_gtf(path): Reads a GENCODE .gtf into memory with gtfparse.
+- get_canonical_cds(gtf): Subsets to Ensembl_canonical CDS features in protein coding genes.
+- annotate_exon_number(cds): Counts the number of CDS exons in each transcript.
+- gtf_to_bed(gtf, id_list): Converts a .gtf file to .bed format.
+- write_bed(bed, path, chr_prefix="chr"): Writes a .bed file to output.
+- canonical_cds(in_file, out_file): Extracts the coding sequence (CDS) for each canonical transcript in the input GTF 
+file, annotates each CDS with its exon number, and writes the results to a BED file.
+- parse_args(): Parses command line arguments.
+- main(): Calls the canonical_cds function with parsed arguments.
+
+Usage:
+- python3 canonical_cds.py --in_file <path_to_input_gtf> --out_file <path_to_output_bed>
+
+Returns:
+- None
 """
 
 # Import modules
@@ -13,13 +30,13 @@ import argparse
 
 # Functions
 def get_gencode_gtf(path):
-    """Read a GENCODE .gtf into memory with gtfparse"""
+    """Read a GENCODE .gtf into memory with gtfparse."""
 
     return gtfparse.read_gtf(path)
 
 
 def get_canonical_cds(gtf):
-    """Subset to Ensembl_canonical CDS features in protein coding genes"""
+    """Subset to Ensembl_canonical CDS features in protein coding genes."""
 
     canonical_cds = gtf[
         (gtf.feature == "CDS")
@@ -31,7 +48,7 @@ def get_canonical_cds(gtf):
 
 
 def annotate_exon_number(cds):
-    """Count the number of CDS exons in each transcript"""
+    """Count the number of CDS exons in each transcript."""
 
     cds["exon_number"] = cds["exon_number"].astype(int)
     cds["cds_number"] = cds.groupby("transcript_id")["exon_number"].rank().astype(int)
@@ -40,12 +57,17 @@ def annotate_exon_number(cds):
 
 
 def gtf_to_bed(gtf, id_list):
-    """Reformat a .gtf to .bed format
-
-    Any identifiers (.gtf file column names) given in "id_list", will be included as
-    a comma-separated string in the bed id column.
     """
+    Convert a .gtf file to .bed format.
 
+    Args:
+        gtf (pandas.DataFrame): A pandas DataFrame in gtf format.
+        id_list (list): A list of identifiers to include in the .bed id column.
+
+    Returns:
+        pandas.DataFrame: A pandas DataFrame in .bed format.
+    """
+    
     # Give desired identifiers in the .bed id column, as a comma-separated string
     gtf["id"] = gtf[id_list].apply(
         lambda row: ",".join(row.values.astype("str")),
@@ -66,8 +88,16 @@ def gtf_to_bed(gtf, id_list):
 
 
 def write_bed(bed, path, chr_prefix="chr"):
-    """Write a .bed file to output.
-    Specify the chr_prefix; either "chr" (GRCh38) or "" (GRCh37).
+    """ 
+    Write a .bed file to output.
+
+    Args:
+    - bed (pandas.DataFrame): A DataFrame in bed format.
+    - path (str): Output file path.
+    - chr_prefix (str): The prefix to add to the seqname column. Default is "chr".
+
+    Returns:
+    - None
     """
 
     # Add or remove the "chr" prefix
@@ -80,8 +110,18 @@ def write_bed(bed, path, chr_prefix="chr"):
     return None
 
 
-def canonical_cds(in_file, outfile):
-    """Run the script"""
+def canonical_cds(in_file, out_file):
+    """
+    Extracts the coding sequence (CDS) for each canonical transcript in the input GTF 
+    file, annotates each CDS with its exon number, and writes the results to a BED file.
+
+    Args:
+        in_file (str): Path to the input GTF file.
+        out_file (str): Path to the output BED file.
+
+    Returns:
+        None
+    """
 
     bed_ids = ["gene_id", "transcript_id", "exon_id", "cds_number"]
 
@@ -90,7 +130,7 @@ def canonical_cds(in_file, outfile):
         .pipe(get_canonical_cds)
         .pipe(annotate_exon_number)
         .pipe(gtf_to_bed, bed_ids)
-        .pipe(write_bed, outfile)
+        .pipe(write_bed, out_file)
     )
 
     return None
@@ -117,10 +157,9 @@ def parse_args():
 
 
 def main():
-    
     args = parse_args()
     canonical_cds(args)
-    
+
     return None
 
 
