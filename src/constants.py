cd62556ca9b0322fd94113be32a29dcafa77e96a
alex <alex.blakes@gmail.com>
2023-10-05 15:10:45 +0100

    Update constants

--- src/constants.py
+++ src/constants.py
@@ -13,15 +13,15 @@ STATISTICS_DIR = f"{DATA_DIR}/statistics"
 # Files
 ## Data
 ### External
-CLINVAR_VARIANT_SUMMARY = "data/external/variant_summary.txt"
-G2P_CARDIAC = "data/external/CardiacG2P_23_8_2023.csv"
-G2P_DD = "data/external/DDG2P_23_8_2023.csv"
-G2P_EYE = "data/external/EyeG2P_23_8_2023.csv"
-G2P_SKELETAL = "data/external/SkeletalG2P_23_8_2023.csv"
-G2P_SKIN = "data/external/SkinG2P_23_8_2023.csv"
-GENCODE_GTF = "data/external/gencode.v39.annotation.gtf"
-GNOMAD_LOEUF_CONSTRAINT = "data/external/supplementary_dataset_11_full_constraint_metrics.tsv"
-OMIM_GENEMAP = "data/external/genemap2.txt"
+CLINVAR_VARIANT_SUMMARY = f"{EXTERNAL_DIR}/variant_summary.txt"
+G2P_CARDIAC = f"{EXTERNAL_DIR}/CardiacG2P_23_8_2023.csv"
+G2P_DD = f"{EXTERNAL_DIR}/DDG2P_23_8_2023.csv"
+G2P_EYE = f"{EXTERNAL_DIR}/EyeG2P_23_8_2023.csv"
+G2P_SKELETAL = f"{EXTERNAL_DIR}/SkeletalG2P_23_8_2023.csv"
+G2P_SKIN = f"{EXTERNAL_DIR}/SkinG2P_23_8_2023.csv"
+GENCODE_GTF = f"{EXTERNAL_DIR}/gencode.v39.annotation.gtf"
+GNOMAD_LOEUF_CONSTRAINT = f"{EXTERNAL_DIR}/supplementary_dataset_11_full_constraint_metrics.tsv"
+OMIM_GENEMAP = f"{EXTERNAL_DIR}/genemap2.txt"
 
 ### Interim
-CANONICAL_CDS_BED = "data/interim/gencode_v39_canonical_cds.bed"
+CANONICAL_CDS_BED = f"{INTERIM_DIR}/gencode_v39_canonical_cds.bed"
