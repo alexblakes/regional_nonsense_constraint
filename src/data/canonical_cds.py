16658f6d36d14f0bcabb34875b552790c9e507e1
alex <alex.blakes@gmail.com>
2023-10-05 15:10:18 +0100

    Use package and module constants

--- src/data/canonical_cds.py
+++ src/data/canonical_cds.py
@@ -1,48 +1,35 @@
 """
-Canonical CDS
+Get CDS features from canonical transcripts in a GENCODE GTF.
 
-This script defines the coordinates of the CDS for canonical transcripts in a GENCODE
-annotation. It takes a GENCODE .gtf file as input. It writes a .bed output for each of 
-the desired features.
+This module extracts the coding sequence (CDS) for each canonical transcript in the 
+input GTF file, annotates annotates the number of CDS exons in each transcript, and 
+writes the results to a BED file.
 
 Functions:
-- get_gencode_gtf(path): Reads a GENCODE .gtf into memory with gtfparse.
-- get_canonical_cds(gtf): Subsets to Ensembl_canonical CDS features in protein coding genes.
-- annotate_exon_number(cds): Counts the number of CDS exons in each transcript.
-- gtf_to_bed(gtf, id_list): Converts a .gtf file to .bed format.
-- write_bed(bed, path, chr_prefix="chr"): Writes a .bed file to output.
-- canonical_cds(in_file, out_file): Extracts the coding sequence (CDS) for each canonical transcript in the input GTF 
-file, annotates each CDS with its exon number, and writes the results to a BED file.
-- parse_args(): Parses command line arguments.
-- main(): Calls the canonical_cds function with parsed arguments.
-
-Usage:
-- python3 canonical_cds.py --in_file <path_to_input_gtf> --out_file <path_to_output_bed>
-
-Returns:
-- None
+    - get_gencode_gtf(path): Reads a GENCODE .gtf into memory with gtfparse.
+    - get_canonical_cds(gtf): Subsets to Ensembl_canonical CDS features in protein coding genes.
+    - annotate_exon_number(gtf): Counts the number of CDS exons in each transcript.
+    - gtf_to_bed(gtf, ids): Converts a .gtf file to .bed format.
+    - write_bed(bed, path, chr_prefix="chr"): Writes a .bed file to output.
+    - main(): Runs all functions in the moduule.
 """
 
-# Import modules
-import pandas as pd
-import gtfparse
-import argparse
-from .. import constants as C
 
-# Module variables
-# FEATURES = ["gene", "transcript", "exon", "CDS"]
-# ATTRIBUTES = ["gene_id", "transcript_id", "gene_name", "exon_id", "exon_number"]
-# BED_COLUMNS = ["seqname", "start", "end", "id", "score", "strand"]
-# BED_IDS = ["gene_id", "transcript_id", "exon_id", "cds_number"]
-# IN_FILE = "data/external/gencode.v39.annotation.gtf"
-# OUT_FILE = "data/interim/gencode_v39_canonical_cds.bed"
-# CHR_PREFIX = "chr"
+# Imports
+import gtfparse
+import pandas as pd
+from src import constants as C
+
+
+# Module constants
+_BED_COLUMNS = ["seqname", "start", "end", "id", "score", "strand"]
+_BED_IDS = ["gene_id", "transcript_id", "exon_id", "cds_number"]
+_CHR_PREFIX = "chr"
 
 
 # Functions
 def get_gencode_gtf(path):
     """Read a GENCODE .gtf into memory with gtfparse."""
-
     return gtfparse.read_gtf(path)
 
 
@@ -73,12 +60,12 @@ def gtf_to_bed(gtf, ids):
 
     Args:
         gtf (pandas.DataFrame): A pandas DataFrame in gtf format.
-        id_list (list): A list of identifiers to include in the .bed id column.
+        ids (list): A list of identifiers to include in the .bed id column.
 
     Returns:
         pandas.DataFrame: A pandas DataFrame in .bed format.
     """
-    
+
     # Place a comma-separated string of identifiers in the .bed id column
     gtf["id"] = gtf[ids].apply(
         lambda row: ",".join(row.values.astype("str")),
@@ -89,17 +76,14 @@ def gtf_to_bed(gtf, ids):
     gtf.loc[:, "score"] = "."
     gtf.loc[:, "start"] = gtf["start"] - 1
     bed = (
-        gtf[BED_COLUMNS]
-        .copy()
-        .sort_values(by=["seqname", "start"])
-        .drop_duplicates()
+        gtf[_BED_COLUMNS].copy().sort_values(by=["seqname", "start"]).drop_duplicates()
     )
 
     return bed
 
 
 def write_bed(bed, path, chr_prefix="chr"):
-    """ 
+    """
     Write a .bed file to output.
 
     Args:
@@ -120,55 +104,16 @@ def write_bed(bed, path, chr_prefix="chr"):
     return None
 
 
-def canonical_cds(in_file, out_file):
-    """
-    Extracts the coding sequence (CDS) for each canonical transcript in the input GTF 
-    file, annotates each CDS with its exon number, and writes the results to a BED file.
-
-    Args:
-        in_file (str): Path to the input GTF file.
-        out_file (str): Path to the output BED file.
-
-    Returns:
-        None
-    """
+def main():
+    """Runs all functions in this module."""
 
     (
-        get_gencode_gtf(in_file)
+        get_gencode_gtf(C.GENCODE_GTF)
         .pipe(get_canonical_cds)
         .pipe(annotate_exon_number)
-        .pipe(gtf_to_bed, BED_IDS)
-        .pipe(write_bed, out_file, CHR_PREFIX)
+        .pipe(gtf_to_bed, _BED_IDS)
+        .pipe(write_bed, C.CANONICAL_CDS_BED, _CHR_PREFIX)
     )
-
-    return None
-
-
-def parse_args():
-    """Parse command line arguments"""
-
-    parser = argparse.ArgumentParser(usage=__doc__)
-    parser.add_argument(
-        "-in_file",
-        type=str,
-        default="data/external/gencode.v39.annotation.gtf",
-        help="Path to GENCODE .gtf file",
-    )
-    parser.add_argument(
-        "-out_file",
-        type=str,
-        default="data/interim/gencode_v39_canonical_cds.bed",
-        help="Path to output .bed file",
-    )
-
-    return parser.parse_args()
-
-
-def main():
-    """Call the canonical_cds function with parsed arguments."""
-    
-    args = parse_args()
-    canonical_cds(args.in_file, args.out_file)
 
     return None
 
