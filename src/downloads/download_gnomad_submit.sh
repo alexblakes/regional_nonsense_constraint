5ce0f00d4b7aeda1e406c40710b03d2708779576
Alex <alex.blakes@gmail.com>
2023-11-08 14:38:10 +0000

    Test gnomad download scripts

--- src/downloads/download_gnomad_submit.sh
+++ src/downloads/download_gnomad_submit.sh
@@ -3,13 +3,13 @@
 # Submission script to download gnomAD data.
 # Run from login node
 
-CHRS=({1..22} X Y)
-# CHRS=({1..2}) # TESTING
+# CHRS=({1..22} X Y)
+CHRS=({1..2}) # TESTING
 
 for CHR in ${CHRS[@]};
     do
         TBI_URL="https://storage.googleapis.com/gcp-public-data--gnomad/release/4.0/vcf/exomes/gnomad.exomes.v4.0.sites.chr"${CHR}".vcf.bgz.tbi"
         VCF_URL="https://storage.googleapis.com/gcp-public-data--gnomad/release/4.0/vcf/exomes/gnomad.exomes.v4.0.sites.chr"${CHR}".vcf.bgz"
-        qsub -b y ~/scratch/ukb_constraint/src/downloads/download_gnomad.sh ${TBI_URL} ${VCF_URL}
+        qsub -b y -cwd ~/scratch/ukb_constraint/src/downloads/download_gnomad.sh ${TBI_URL} ${VCF_URL}
         # bash ~/scratch/ukb_constraint/src/downloads/download_gnomad.sh ${TBI_URL} ${VCF_URL}
     done
