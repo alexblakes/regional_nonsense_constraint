2fd4eebccb93928421f5be3a907d2dc00756b603
Alex <alex.blakes@gmail.com>
2023-11-09 16:03:44 +0000

    Download constraint README

--- src/downloads/download_gnomad_constraint.sh
+++ src/downloads/download_gnomad_constraint.sh
@@ -9,6 +9,7 @@
 # Constants
 DIR="/mnt/bmh01-rds/Ellingford_gene/public_data_resources/gnomad/v4.0/constraint"
 URL="https://storage.googleapis.com/gcp-public-data--gnomad/release/v4.0/constraint/gnomad.v4.0.constraint_metrics.tsv"
-
+README_URL="https://storage.googleapis.com/gcp-public-data--gnomad/release/v4.0/constraint/README.txt"
 # Download
-wget -P ${DIR} ${URL}
+wget -c -P ${DIR} ${URL}
+wget -c -P ${DIR} ${README_URL}
