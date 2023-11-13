03058ad82431cf7f4de1c7f037d48bc1b0786490
Alex <alex.blakes@gmail.com>
2023-11-13 16:20:48 +0000

    Combine pass SNVs

--- src/data/combine_pass_snvs.sh
+++ src/data/combine_pass_snvs.sh
@@ -1,25 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# This script downloads and combines all the UKB variants passing gnomAD filters
-
-# Get filepaths readable to dx download
-find "/mnt/project/outputs/gnomad_pass_variants/split_pass_variants/" -type f |\
-sed 's/^.\{13\}//' |\
-sort >\
-pass_snps_file_paths_dx.txt
-
-# Use dx download to download the files locally
-# head -n 100 pass_snps_file_paths_dx.txt > test_paths.txt
-rm -rf pass_snv_files/
-mkdir -p pass_snv_files/
-# Takes ~ 1 hour to run
-xargs -a pass_snps_file_paths_dx.txt -n 10 -P 0 -I % dx download -o pass_snv_files/ %
-
-
-# Concatenate the individual files containing PASS SNVs
-# The number of files is too great for cat, so passing to xargs is required
-find pass_snv_files/ -type f -name *.tsv | xargs -n 1000 -I % cat % > all_pass_snvs.txt
-
-# Upload the combined data
-dx rm -f outputs/gnomad_pass_variants/all_pass_snvs.txt
-dx upload --destination outputs/gnomad_pass_variants/ all_pass_snvs.txt
+# Concatenate the gnomAD pass SNVs
+# The jobs waits until all bcftools scripts have completed
+qsub -b y -cwd -N "concatenate_pass_variants" -e $LOG_DIR -o $LOG_DIR \
+    -hold_jid "extract_pass_variants_*" \
+    cat "data/scratch/gnomad_v4_*_snps_pass.tsv" > data/interim/gnomad_v4_pass_snvs.tsv
