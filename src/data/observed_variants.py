a6db7c43b6b2b7eaafea9d5d1b0f0da79ea38168
Alex <alex.blakes@gmail.com>
2023-11-15 16:15:57 +0000

    Merge and tidy variant annotations

--- src/data/observed_variants.py
+++ src/data/observed_variants.py
@@ -29,21 +29,23 @@ logger = setup_logger(Path(__file__).stem)
 
 
 # Functions
-def get_observed_variants(path):
-    """Get observed variants in gnomAD v4.0"""
 
-    logger.info("Getting observed variants in gnomAD v4.0.")
 
-    obs = pd.read_csv(
+def get_trinucleotide_contexts(path):
+    """Get trinucleotide contexts."""
+
+    logger.info("Getting trinucleotide sequence contexts.")
+
+    tri = pd.read_csv(
         path,
         sep="\t",
-        header=None,
-        names=_VCF_HEADER + ["ac", "an"],
-        usecols=["chr", "pos", "ref", "alt", "ac", "an"],
         dtype=_DATATYPES,
-    ).assign(obs=1)
+        # nrows=10000,  #! Testing
+    )
 
-    return obs
+    logger.info(f"Positions with trinucleotide sequence contexts: {len(tri)}")
+
+    return tri
 
 
 def get_vep_annotations(path):
@@ -56,15 +58,56 @@ def get_vep_annotations(path):
         sep="\t",
         comment="#",
         header=None,
-        names=["chr","pos","ref","alt","csq","enst"],
+        names=["chr", "pos", "ref", "alt", "csq", "enst"],
         dtype=_DATATYPES,
-        nrows = 100 # ! Testing
+        # nrows=10000  # ! Testing
         # usecols=["chr", "pos", "ref", "alt", "info"],
     )
 
     logger.info(f"VEP-annotated possible SNVs: {len(vep)}")
 
     return vep
+
+
+def get_nmd_annotations(path):
+    """Get NMD annotations for all CDS sites."""
+
+    logger.info("Getting NMD annotations for all CDS sites.")
+
+    nmd = pd.read_csv(
+        path,
+        sep="\t",
+        usecols=["chr", "pos", "transcript_id", "nmd_definitive"],
+        dtype=_DATATYPES,
+        # nrows=10000,  # ! Testing
+    ).rename(columns={"transcript_id": "enst", "nmd_definitive": "nmd"})
+
+    logger.info(f"NMD annotations: {len(nmd)}")
+
+    return nmd
+
+
+def get_observed_variants(path):
+    """Get observed variants in gnomAD v4.0"""
+
+    logger.info("Getting observed variants in gnomAD v4.0.")
+
+    obs = pd.read_csv(
+        path,
+        sep="\t",
+        header=None,
+        names=_VCF_HEADER + ["ac", "an"],
+        usecols=["chr", "pos", "ref", "alt", "ac", "an"],
+        dtype=_DATATYPES,
+        # nrows=10000,  #! Testing
+    ).assign(obs=True)
+
+    logger.info(f"Observed variants: {len(obs)}")
+    logger.info(
+        f"Observed variant duplicates: {obs.duplicated(['chr','pos', 'ref','alt']).sum()}"
+    )
+
+    return obs
 
 
 def get_methylation_data(path):
@@ -78,29 +121,69 @@ def get_methylation_data(path):
         header=0,
         names=["ix", "chr", "pos", "alleles", "lvl"],
         usecols=["chr", "pos", "lvl"],
+        # nrows=10000,  # ! Testing
     )
+
+    logger.info(f"CpG methylation annotations: {len(meth)}")
 
     return meth
 
+def tidy_df(df):
+    """Tidy dataframe and log summary data."""
+ 
+    # Assign methylation levels. 
+    # All non-CpG sites have methylation level 0.
+    logger.info(f"Missing lvl annotations: {df['lvl'].isna().sum()}")
 
-def get_nmd_annotations(path):
-    """Get NMD annotations for all CDS sites."""
+    df.loc[df["variant_type"] != "CpG", "lvl"] = 0
+    df.lvl = df.lvl.astype(int) # For later merging with mutability data
 
-    logger.info("Getting NMD annotations for all CDS sites.")
+    logger.info(f"Missing lvl annotations after assigning non-CpGs to lvl = 0: {df['lvl'].isna().sum()}")
+    
+    # Missing values
+    df = df.dropna(subset="nmd")
 
-    nmd = pd.read_csv(
-        path,
-        sep="\t",
-        usecols=["chr", "pos", "transcript_id", "nmd_definitive"],
-    ).rename(columns={"transcript_id": "enst", "nmd_definitive": "nmd"})
-    return nmd
+    logger.info(f"Variants with an NMD annotation: {len(df)}")
 
+    logger.info(f"These sites, which lack a variant_type annotation, are dropped:\n{df[df['variant_type'].isna()]}")
+    df = df.dropna(subset="variant_type")
+
+    # Drop chrM sites
+    logger.info(f"Unique chromosomes: {df.chr.nunique()}")
+
+    m = df[df["chr"] == "chrM"]
+
+    logger.info(f"chrM sites: {len(m)}")
+    logger.info(f"Observed chrM variants: {m.obs.sum()}")
+
+    df = df[df["chr"] != "chrM"]
+
+    logger.info(f"chrM sites are dropped.")
+    logger.info(f"Variants after dropping chrM sites: {len(df)}")
+
+    # Summary statistics (all variants)
+    logger.info(f"Unique transcripts: {df.enst.nunique()}")
+    logger.info(f"Consequence value counts:\n{df.csq.value_counts()}")
+    logger.info(f"CpG value counts:\n{df.variant_type.value_counts()}")
+    logger.info(f"NMD regions value counts:\n{df.nmd.value_counts()}")
+    
+    # Summary statistics (CpGs)
+    cpg = df[df["variant_type"] == "CpG"]
+
+    logger.info(f"CpG levels:\n{cpg.lvl.value_counts()}")
+
+    # Summary statistics (Observed variants)
+    obs = df[df["obs"] == True]
+    
+    logger.info(f"Observed variants consequence value counts:\n{obs.csq.value_counts()}")
+ 
+    return df
 
 def main():
     """Run the script."""
 
     vep = get_vep_annotations(C.VEP_ALL_SNVS_TIDY)
-    tri = pd.read_csv(C.CDS_ALL_SNVS_TRI_CONTEXT, sep="\t", dtype=_DATATYPES)
+    tri = get_trinucleotide_contexts(C.CDS_ALL_SNVS_TRI_CONTEXT)
     nmd = get_nmd_annotations(C.NMD_ANNOTATIONS)
     obs = get_observed_variants(C.GNOMAD_PASS_SNVS)
     meth = get_methylation_data(C.GNOMAD_NC_METHYLATION)
@@ -108,16 +191,20 @@ def main():
     variant_types = mu[["tri", "ref", "alt", "variant_type"]].drop_duplicates()
 
     # Merge VEP, context, NMD, and observed variant annotations
+    logger.info("Merging annotations.")
+
     df = (
         vep.merge(tri, how="left")
         .merge(nmd, how="left")
         .merge(obs, how="left")
-        .fillna({"obs": 0})
+        .fillna({"obs": False})
         .merge(variant_types, how="left")
         .merge(meth, how="left")
     )
 
-    return df  # TODO Testing
+    logger.info(f"Variants in raw merged data: {len(df)}")
+
+    return df  #! Testing
 
 
 if __name__ == "__main__":
