1de4dd9030c8d04afd50d66f7f42b050b678d3cb
Alex <alex.blakes@gmail.com>
2023-11-17 15:52:00 +0000

    Get constraint statistics

--- src/constraint/expected_variants.py
+++ src/constraint/expected_variants.py
@@ -45,7 +45,7 @@ def process_synonymous_variants(syn, cpg=False):
 
 def variant_expectation_model(df):
     """Variant expectation model.
-    
+
     Predicts the proportion of possible variants observed, for a given mutability.
     """
 
@@ -60,9 +60,10 @@ def variant_expectation_model(df):
 
     return results
 
+
 def reindex_non_cpgs(df):
     """Ensure all combinations of enst / region / csq are present."""
-    
+
     logger.info(f"Re-indexing variants: {df.variant_type.unique()}")
     logger.info(f"Number of unique transcripts: {df.enst.nunique()}")
 
@@ -75,10 +76,64 @@ def reindex_non_cpgs(df):
 
     # Check that all combinations of enst, region, and csq are present
     logger.info(f"Number of rows: {len(df)}")
-    logger.info(f"Number of combinations of enst/region/csq: {df.enst.nunique() * df.region.nunique() * df.csq.nunique()}")
+    logger.info(
+        f"Number of combinations of enst/region/csq: {df.enst.nunique() * df.region.nunique() * df.csq.nunique()}"
+    )
 
     # Fill NaN values for variant type
     df["variant_type"] = df["variant_type"].fillna("non-CpG")
+
+    return df
+
+
+def predict_expected_variants(df, fit_model):
+    """Get the proportion and number of variants expected."""
+
+    # Predict the proportion of variants observed
+    x = df["mu"]
+    X = sm.tools.add_constant(x)
+    df["prop_exp"] = 1 - np.exp(fit_model.predict(X))
+
+    # Calculate the number of variants expected
+    df["n_exp"] = (df["prop_exp"] * df["pos"]).pipe(np.round, 3)
+
+    return df
+
+
+def combine_constraint_statistics_for_all_regions(non_cpg, cpg):
+    """Combine results for CpG and non-CpG variants."""
+
+    # Give CpG and non-CpG variants a consistent index
+    non_cpg = non_cpg.set_index(["enst", "region", "csq"])
+    cpg = cpg.set_index(["enst", "region", "csq"])
+
+    # Combine non-CpG and CpG data into the essential summary statistics
+    n_pos = non_cpg["pos"].fillna(0) + cpg["pos"].fillna(0)
+    n_obs = non_cpg["obs"].fillna(0) + cpg["obs"].fillna(0)
+    n_exp = non_cpg["n_exp"].fillna(0) + cpg["n_exp"].fillna(0)
+    oe = (n_obs / n_exp).rename("oe")
+    prop_obs = (n_obs / n_pos).rename("prop_obs")
+    prop_exp = (n_exp / n_pos).rename("prop_exp")
+
+    # Calculate the total mutability for each region
+    # In each dataframe, "mu" is the mean mutability for contexts in a region
+    mu_non_cpg = (non_cpg["mu"] * non_cpg["pos"]).fillna(0)
+    mu_cpg = (cpg["mu"] * cpg["pos"]).fillna(0)
+    mu = (mu_non_cpg + mu_cpg).rename("mu")
+
+    # Combine the summary statistics
+    df = pd.concat(
+        [
+            n_pos,
+            n_obs,
+            n_exp,
+            oe,
+            prop_obs,
+            prop_exp,
+            mu,
+        ],
+        axis=1,
+    ).reset_index(drop=False)
 
     return df
 
@@ -88,7 +143,7 @@ def main():
 
     # Read synonymous counts
     syn = pd.read_csv(C.OBS_COUNTS_SYN_20, sep="\t")
-    
+
     # Get proportion observed
     syn["prop_obs"] = syn["obs"] / syn["pos"]
 
@@ -98,39 +153,31 @@ def main():
     syn_non = process_synonymous_variants(syn, cpg=False)
     syn_cpg = process_synonymous_variants(syn, cpg=True)
 
+    # Get separate expectation models for CpGs and non-CpGs
     non_cpg_results = variant_expectation_model(syn_non)
     cpg_results = variant_expectation_model(syn_cpg)
 
-    # syn_non = reindex_non_cpgs(syn_non)
+    # Get observed variants by region
+    region = pd.read_csv(C.OBS_COUNTS_REGIONS_20_CLEAN, sep="\t")
+    non_cpg = (
+        region[region.variant_type == "non-CpG"]
+        .copy()
+        .pipe(predict_expected_variants, non_cpg_results)
+    )
+    cpg = (
+        region[region.variant_type == "CpG"]
+        .copy()
+        .pipe(predict_expected_variants, cpg_results)
+    )
 
-    return syn_non  #! Testing
+    # Get constraint statistics for all regions
+    df = combine_constraint_statistics_for_all_regions(non_cpg=non_cpg, cpg=cpg)
+
+    return df  #! Testing
 
 
 if __name__ == "__main__":
     main()
-
-# # ## Calculate the expected proportion of variants per transcript and region
-
-
-# # ### Non-CpGs
-
-
-# # Predict the proportion observed with the non-CpG model
-# non_cpg["prop_exp"] = 1 - np.exp(non_cpg_results.predict(sm.tools.add_constant(non_cpg["mu"])))
-
-# # Calculate the number of variants expected
-# non_cpg["n_exp"] = (non_cpg["prop_exp"] * non_cpg["n_pos"]).pipe(np.round, 3)
-
-
-# # ### CpGs
-
-
-# # Predict the proportion observed with the CpG model
-# cpg["prop_exp"] = 1 - np.exp(cpg_results.predict(np.exp(sm.tools.add_constant(cpg["mu"]))))
-
-# # Calculate the number of variants expected
-# cpg["n_exp"] = (cpg["prop_exp"] * cpg["n_pos"]).pipe(np.round, 3)
-
 
 # # ### Combine CpG and non-CpG variants
 
