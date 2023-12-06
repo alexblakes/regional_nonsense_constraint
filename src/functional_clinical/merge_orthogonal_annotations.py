3c2646ddc535b47ee6ffac7d9333774be8880771
Alex <alex.blakes@gmail.com>
2023-12-06 16:02:37 +0000

    Get mean scores

--- src/functional_clinical/merge_orthogonal_annotations.py
+++ src/functional_clinical/merge_orthogonal_annotations.py
@@ -1,7 +1,6 @@
 """Merge NMD, phyloP, AlphaMissense, and pext annotations."""
 
 # Imports
-from locale import D_T_FMT
 from pathlib import Path
 
 import numpy as np
@@ -9,11 +8,9 @@ import pandas as pd
 
 from src import constants as C
 from src import setup_logger
-from src.functional_clinical.alpha_missense_tidy import read_alpha_missense
-
 
 # Module constants
-
+_METRICS = ["phylop", "pext", "alpha_mis"]
 
 # Logging
 logger = setup_logger(Path(__file__).stem)
@@ -93,7 +90,7 @@ def read_alpha_missense(path):
         path,
         sep="\t",
         nrows=10000,  #! Testing
-    )
+    ).rename(columns={"alpha_missense_min": "alpha_mis"})
 
     logger.info(f"AlphaMissense annotations: {len(am)}")
 
@@ -108,56 +105,45 @@ def read_regional_nonsense_constraint(path):
     return constraint
 
 
-def se(p, n):
-    """Calculate the standard error of a propotion."""
-    return np.sqrt((p * (1 - p)) / n)
+def get_mean_score(df, metric, group_by_constraint=False):
+    """Find the mean score of the annotation in NMD regions."""
 
-def find_sites_meeting_condition(df, annotation, condition):
-    """Find sites at which the values for an annotation meet the given condition."""
-
-    df = df[["region", "constraint", annotation]].copy().dropna(subset=annotation)
-    df[annotation] = condition(df[annotation])
-
-    return df
-
-def get_proportion_meeting_condition(df, group_on_constraint=False):
-    """Find the proportion of sites which fulfil the condition."""
-
-    annotation = df.columns.drop(["region","constraint"])[0]
-
-    if not group_on_constraint:
+    if not group_by_constraint:
         df = df.groupby("region")
-    elif group_on_constraint:
-        df = df.groupby(["region", "constraint"])
+    elif group_by_constraint:
+        df = df.groupby(["region", "constraint"], dropna=False)
 
-    df = df[annotation].agg(["mean","count"]).set_axis([annotation,"n"], axis=1).reset_index(drop=False)
+    df = (
+        df[metric]
+        .agg(["mean", "count", "sem"])
+        .set_axis([metric, "n", "sem"], axis=1)
+        .assign(ci_95=lambda x: x["sem"] * 1.96)
+        .reset_index(drop=False)
+    )
 
-    if not group_on_constraint:
-        df.assign(constraint="full_transcript")
+    if not group_by_constraint:
+        df = df.assign(constraint="all")
+
+    df = df.melt(
+        id_vars=["region", "n", "sem", "ci_95", "constraint"],
+        var_name="metric",
+        value_name="mean",
+    )[["region", "constraint", "metric", "mean", "n", "sem", "ci_95"]].sort_values(
+        ["constraint", "region"]
+    )
 
     return df
 
-def get_95_ci(df):
-    
 
-# # Regions, ignoring constraint annotation
-# r = (
-#     p.groupby("region")
-#     .agg(phylop=("phylop","mean"), n=("phylop","count"))
-#     .assign(constraint="all")
-#     .reset_index(drop=False)
-# )
+def combine_mean_scores_across_groups(df, annotation):
+    """Concatenate mean scores for regions and constraint groups."""
+    return pd.concat(
+        [
+            get_mean_score(df, annotation, False),
+            get_mean_score(df, annotation, True),
+        ]
+    )
 
-# # Regions and constraint annotation
-# rc = (
-#     p.groupby(["region","constraint"])
-#     .agg(phylop=("phylop","mean"), n=("phylop","count"))
-#     .reset_index(drop=False)
-# )
-
-# # Combine
-# _p = pd.concat([rc,r])
-# _p["ci95"] = 1.96 * se(_p["phylop"], _p["n"])
 
 def main():
     """Run as script."""
@@ -192,7 +178,7 @@ def main():
     df = df.merge(am, how="left")
     logger.info(f"Sites after merging with AlphaMissense scores: {len(df)}")
     logger.info(
-        f"Sites with an AlphaMissense score: {len(df) - df.alpha_missense_min.isna().sum()}"
+        f"Sites with an AlphaMissense score: {len(df) - df.alpha_mis.isna().sum()}"
     )
 
     ## Constraint annotations
@@ -208,115 +194,16 @@ def main():
         f"Sites per region and constraint annotation:\n{df.groupby('region').constraint.value_counts(dropna=False)}"
     )
 
-    # Statistics for each annotation
-    _p = find_sites_meeting_condition(df, "phylop", lambda x: x > 2.27)
-    _p = pd.concat([get_proportion_meeting_condition(_p), get_proportion_meeting_condition(_p, True)])
+    # Mean scores for each annotation
+    stats = pd.concat([combine_mean_scores_across_groups(df, x) for x in _METRICS])
 
+    # Write to output
+    logger.info("Writing to output.")
+    df.to_csv(C.CDS_PHYLOP_PEXT_MISSENSE, sep="\t", index=False)
+    stats.to_csv(C.PHYLOP_PEXT_MISSENSE_STATS, sep="\t", index=False)
 
-
-    return _p  #! Testing
+    return stats  #! Testing
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-# # ## Statistics
-
-
-# # ### phyloP
-# # Calculate the proportion of sites in each grouping with phyloP > 2.27
-
-
-# # Find sites where phyloP > 2.27
-# p = df[["region", "phylop", "constraint"]].copy().dropna(subset="phylop")
-# p["phylop"] = np.where(p["phylop"] > 2.27, 1, 0)
-
-
-# # Regions, ignoring constraint annotation
-# r = (
-#     p.groupby("region")
-#     .agg(phylop=("phylop","mean"), n=("phylop","count"))
-#     .assign(constraint="all")
-#     .reset_index(drop=False)
-# )
-
-# # Regions and constraint annotation
-# rc = (
-#     p.groupby(["region","constraint"])
-#     .agg(phylop=("phylop","mean"), n=("phylop","count"))
-#     .reset_index(drop=False)
-# )
-
-# # Combine
-# _p = pd.concat([rc,r])
-# _p["ci95"] = 1.96 * se(_p["phylop"], _p["n"])
-
-# # Write out
-# _p.to_csv("../outputs/stats_phylop.tsv", sep="\t", index=False)
-
-
-# # ### HMC
-# # Calculate the proportion of sites in each group where HMC < 1
-
-
-# # Find sites where HMC < 1
-# h = df[["region", "hmc", "constraint"]].copy().dropna(subset="hmc")
-# h["hmc"] = np.where(h["hmc"] < 1, 1, 0)
-
-
-# # Regions, ignoring constraint annotation
-# r = (
-#     h.groupby("region")
-#     .agg(hmc=("hmc","mean"), n=("hmc","count"))
-#     .assign(constraint="all")
-#     .reset_index(drop=False)
-# )
-
-# # Regions and constraint annotation
-# rc = (
-#     h.groupby(["region","constraint"])
-#     .agg(hmc=("hmc","mean"), n=("hmc","count"))
-#     .reset_index(drop=False)
-# )
-
-# # Combine
-# _h = pd.concat([rc,r])
-# _h["ci95"] = 1.96 * se(_h["hmc"], _h["n"])
-
-# # Write out
-# _h.to_csv("../outputs/stats_hmc.tsv", sep="\t", index=False)
-
-
-# # ### pext
-# # Calculate the mean pext of sites in each group
-
-
-# # Find sites with a pext annotation
-# x = df[["region", "pext", "constraint"]].copy().dropna(subset="pext")
-
-
-# # Regions, ignoring constraint annotation
-# r = (
-#     x.groupby("region")
-#     .agg(pext=("pext", "mean"), n=("pext","count"), sem=("pext", "sem"))
-#     .assign(constraint="all")
-#     .reset_index(drop=False)
-# )
-
-# # Regions and constraint annotation
-# rc = (
-#     x.groupby(["region","constraint"])
-#     .agg(pext=("pext", "mean"), n=("pext","count"), sem=("pext", "sem"))
-#     .reset_index(drop=False)
-# )
-
-# # Combine
-# _x = pd.concat([rc,r])
-# _x["ci95"] = 1.96 * _x["sem"]
-# _x = _x.drop("sem", axis=1)
-
-# # Write out
-# _x.to_csv("../outputs/stats_pext.tsv", sep="\t", index=False)
