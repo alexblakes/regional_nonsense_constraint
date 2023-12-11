dc893c090f34e5206ac7eb242187d8dd99073252
Alex <alex.blakes@gmail.com>
2023-12-11 10:30:19 +0000

    Grep for canonical transcripts

--- src/data/vep_all_snvs_combined.sh
+++ src/data/vep_all_snvs_combined.sh
@@ -8,13 +8,13 @@
 # 3. The "location" column is split into "chr" and "pos" columns
 # 4. Consequences are sanitised. For example "missense_variant,splice_region_variant" 
 #    becomes "missense_variant".
+# 5. Only variants in the canonical transcript list are kept.
 #
 # Save the result to a TSV.
 
 cat data/interim/vep_all_snvs/out_*.tsv | \
-head -n 100000 | \
 grep -v '^#' | \
-grep -f data/interim/transcript_ids.txt | \
+grep -f data/interim/transcript_ids.tsv | \
 grep -E 'stop_gained|missense|synonymous' | \
 awk -F '[:\t]' -v OFS='\t' '{ $1=$1; print $0 }' | \
 awk -F '\t' -v OFS='\t' '{ sub(".*missense.*", "missense_variant", $5); print $0 }' | \
