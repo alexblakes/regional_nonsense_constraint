1c77376213337026192eb51611d796d3fc980a77
Alex <alex.blakes@gmail.com>
2024-01-22 15:19:14 +0000

    Save conservation plots

--- src/visualisation/phylop_plots.py
+++ src/visualisation/phylop_plots.py
@@ -4,22 +4,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from src import constants as C
+from src import visualisation as vis
+
+# Styles and palettes
+plt.style.use(C.STYLE_DEFAULT)
+_PALETTE = vis.color_palette("regions")[::-1]
+
 
 # Functions
-def horizontal_bars(df, ax=None, width, xlabel=False, yticklabels=[], **kwargs):
+def horizontal_bars(
+    values,
+    ax=None,
+    **kwargs,
+):
+    
+    kwargs.setdefault("tick_label", values.index)
+    kwargs.setdefault("color", _PALETTE)
+    kwargs.setdefault("ecolor", [vis.adjust_lightness(c, 0.8) for c in _PALETTE])
+
     if not ax:
         ax = plt.gca()
-    
-    n = len(df)  # Number of bars
+
+    n = len(values)  # Number of bars
     y = np.arange(n)  # Y ticks
-    w = df[width]  # Width of bars (x-axis value)
 
-    ax.bar(y=y / n, width=w, height=1 / (n + 1), **kwargs)
-
-    ax.set_yticks(ticks=y * 1 / n, labels=yticklabels)
-    ax.tick_params(axis="y", length=0)
-
-    if xlabel:
-        ax.set_xlabel(xlabel)
+    ax.barh(y=y / n, width=values, height=1 / (n + 1), **kwargs)
 
     return None
