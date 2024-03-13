a085e30f98e830d86f751835eea5c2c60bb803c9
Alex <alex.blakes@gmail.com>
2024-03-13 11:21:12 +0000

    Refactor src logging

--- src/__init__.py
+++ src/__init__.py
@@ -4,34 +4,51 @@ import logging
 
 from src import constants as C
 
+# Module constants
+_FORMATTER = logging.Formatter(
+    fmt="[%(asctime)s] %(levelname)s %(name)s %(funcName)s(): %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+)
+
 
 # Functions
-def setup_logger(name, propagate=True, logfile=None,):
-    """Create a new logger."""
+def add_filehandler(logger, logfile, level=logging.DEBUG):
     
+    file_handler = logging.FileHandler(logfile)
+    file_handler.setLevel(level)
+    file_handler.setFormatter(_FORMATTER)
+    logger.addHandler(file_handler)
+
+    return logger
+
+def add_streamhandler(logger, level=logging.INFO):
+    
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(level)
+    stream_handler.setFormatter(_FORMATTER)
+    logger.addHandler(stream_handler)
+    
+    return logger
+
+def setup_logger(
+    name,
+    propagate=True,
+    logfile=None,
+):
+    """Create a new logger."""
+
     logger = logging.getLogger(name)
     logger.propagate = propagate
     logger.setLevel(logging.DEBUG)
 
-    formatter = logging.Formatter(
-        fmt="[%(asctime)s] %(levelname)s %(name)s %(funcName)s(): %(message)s",
-        datefmt="%d-%b-%y %H:%M:%S",
-    )
-
     # Log to file
     if logfile:
-        file_handler = logging.FileHandler(logfile)
-        file_handler.setLevel(logging.DEBUG)
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+        logger = add_filehandler(logger, logfile)
 
     # Log to std err
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.INFO)
-    stream_handler.setFormatter(formatter)
-    logger.addHandler(stream_handler)
+    logger = add_streamhandler(logger)
 
     return logger
 
-root_logger = setup_logger("", logfile="data/logs/_all_python_modules.log")
 
+root_logger = setup_logger("", logfile="data/logs/_all_python_modules.log")
