13c6bbbd7b178b3b174eef5a6ff86f1516eb1bfd
Alex <alex.blakes@gmail.com>
2024-03-18 14:09:08 +0000

    Update boilerplate code

--- src/boilerplate.py
+++ src/boilerplate.py
@@ -1,17 +1,26 @@
-"""Boilerplate code for logging in all modules."""
+"""Boilerplate code for most modules."""
 
+# Imports
 import logging
 from pathlib import Path
+
 import src
+from src import constants as C
+
 
 # Module constants
 _LOGFILE = f"data/logs/{Path(__file__).stem}.log"
 
+
 # Logging
 logger = logging.getLogger(__name__)
 
-# Functions
 
-# if __name__ == "__main__" statement
+# Functions
+def main():
+    pass
+
+
 if __name__ == "__main__":
     logger = src.module_logger(_LOGFILE)
+    main()
---
+++ src/cadd/cadd_plots.py
@@ -0,0 +1,19 @@
+"""Plot CADD score summary data."""
+
+import logging
+from pathlib import Path
+import src
+
+# Module constants
+_LOGFILE = f"data/logs/{Path(__file__).stem}.log"
+
+# Logging
+logger = logging.getLogger(__name__)
+
+# Functions
+def main()
+    pass
+
+if __name__ == "__main__":
+    logger = src.module_logger(_LOGFILE)
+    main()
