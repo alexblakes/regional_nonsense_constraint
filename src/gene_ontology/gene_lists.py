85b2f0fdb566811bce110e5a5d43cf7b0cc673d7
Alex <alex.blakes@gmail.com>
2024-03-28 11:21:05 +0000

    Finalise gene_lists.py script

--- src/gene_ontology/gene_lists.py
+++ src/gene_ontology/gene_lists.py
@@ -1,6 +1,5 @@
-"""Module docstring."""
+"""Get constrained gene lists."""
 
-# Imports
 import logging
 from pathlib import Path
 
@@ -10,40 +9,63 @@ import src
 from src import constants as C
 from src.constraint import constraint_statistics
 
-# Module constants
 _LOGFILE = f"data/logs/{Path(__file__).stem}.log"
 
-
-# Logging
 logger = logging.getLogger(__name__)
 
 
-# Functions
 def read_gene_ids(path):
     return pd.read_csv(path, sep="\t", usecols=["gene_id", "transcript_id"]).set_axis(
         ["ensg", "enst"], axis="columns"
     )
 
 
-def get_constrained_gnomad_genes(gnomad, gene_ids):
+def get_constrained_gnomad_transcripts(gnomad):
+    logger.info("Getting constrained gnomAD transcripts.")
 
-    # Find constrained transcripts
     m1 = gnomad.pli > 0.9
     m2 = gnomad.loeuf < 0.6
 
     gnomad_strong = gnomad[m1 | m2]["enst"].drop_duplicates()
 
-    logger.info(f"gnomAD constrained transcripts: {len(gnomad_strong)}")
-
-    # Get gene ids
-    gnomad_strong = gene_ids.merge(gnomad_strong, how="inner", validate="one_to_one")[
-        "ensg"
-    ]
-    assert gnomad_strong.duplicated().sum() == 0, "Duplicated ENSG IDs."
-
-    logger.info(f"gnomAD constrained gene ids: {len(gnomad_strong)}")
+    logger.info(f"Constrained transcripts in gnomAD: {len(gnomad_strong)}")
 
     return gnomad_strong
+
+
+def get_gene_ids(df, gene_ids):
+    """Get gene IDs for a dataframe with a list of transcript IDs."""
+
+    df = gene_ids.merge(df, how="inner", on="enst", validate="one_to_one")["ensg"]
+    assert df.duplicated().sum() == 0, "Duplicated gene IDs."
+
+    logger.info(f"Gene ids: {len(df)}")
+
+    return df
+
+
+def get_regional_constrained_genes(
+    path=C.REGIONAL_NONSENSE_CONSTRAINT, region="nmd_target"
+):
+    logger.info(region)
+
+    rnc = (
+        pd.read_csv(
+            path,
+            sep="\t",
+            usecols=["enst", "region", "constraint"],
+        )
+        .query("region == @region")
+        .query('constraint == "constrained"')
+    )
+
+    assert (
+        rnc.enst.duplicated().sum() == 0
+    ), f"Duplicated transcript IDs in {region} region."
+
+    assert len(rnc) > 0, "Empty dataframe."
+
+    return rnc
 
 
 def main():
@@ -59,15 +81,31 @@ def main():
     logger.info(f"All genes: {len(_all)}")
 
     ## Constrained genes in gnomAD
-    gnomad_strong = get_constrained_gnomad_genes(gnomad, gene_ids)
+    gnomad_strong = get_constrained_gnomad_transcripts(gnomad).pipe(
+        get_gene_ids, gene_ids
+    )
 
-
+    ## Genes with regional nonsense constraint
+    nmd_target = get_regional_constrained_genes().pipe(get_gene_ids, gene_ids)
+    start_proximal = get_regional_constrained_genes(region="start_proximal").pipe(
+        get_gene_ids, gene_ids
+    )
+    long_exon = get_regional_constrained_genes(region="long_exon").pipe(
+        get_gene_ids, gene_ids
+    )
+    distal = get_regional_constrained_genes(region="distal_nmd").pipe(
+        get_gene_ids, gene_ids
+    )
 
     # Write to output
     _all.to_csv(C.GENE_LIST_ALL, index=False, header=None)
     gnomad_strong.to_csv(C.GENE_LIST_GNOMAD_CST, index=False, header=None)
+    nmd_target.to_csv(C.GENE_LIST_NMD_TARGET, index=False, header=None)
+    start_proximal.to_csv(C.GENE_LIST_START_PROX, index=False, header=None)
+    long_exon.to_csv(C.GENE_LIST_LONG_EXON, index=False, header=None)
+    distal.to_csv(C.GENE_LIST_DISTAL, index=False, header=None)
 
-    return gene_ids, gnomad, gnomad_strong
+    return None
 
 
 if __name__ == "__main__":
