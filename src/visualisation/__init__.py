79f00f20445780613df84f44c25550252da249fa
Alex <alex.blakes@gmail.com>
2024-04-04 17:28:16 +0100

    Add adjust_alpha function

--- src/visualisation/__init__.py
+++ src/visualisation/__init__.py
@@ -21,9 +21,9 @@ logger = setup_logger(Path(__file__).stem)
 def color_palette(style="default"):
     """Choose a color palette."""
 
-    if not style in ["default","regions", "maps"]:
+    if not style in ["default", "regions", "maps"]:
         raise ValueError("style must be one of 'default', 'regions', or 'maps'.")
-    
+
     if style == "default":
         labels = "blue green orange red light_blue pink grey black"
         plt.style.use(C.COLOR_VIBRANT)
@@ -52,6 +52,10 @@ def adjust_lightness(color, amount=0.5):
     c = colorsys.rgb_to_hls(*mc.to_rgb(c))
 
     return colorsys.hls_to_rgb(c[0], max(0, min(1, amount * c[1])), c[2])
+
+
+def adjust_alpha(color, alpha):
+    return mc.to_rgba(color, alpha)
 
 
 def panel_label(ax, s, x=-0.05, y=1.05, **kwargs):
