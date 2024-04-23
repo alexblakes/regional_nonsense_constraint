64eda931cb00a7d04eb9c109fb5e4420722339a7
Alex <alex.blakes@gmail.com>
2024-04-23 10:54:07 +0100

    Run WebGestaltR

--- src/gene_enrichment/webgestalt.R
+++ src/gene_enrichment/webgestalt.R
@@ -2,17 +2,11 @@
 
 library(WebGestaltR)
 
-# enrichDatabase options: 
-#   - phenotype_Human_Phenotype_Ontology
-#   - geneontology_Biological_Process_noRedundant
-#   - geneontology_Molecular_Function_noRedundant
-
-run_ora <- function(interestGenes, referenceGenes) {
-
-    WebGestaltR(
+run_ora <- function(interestGenes, referenceGenes, database) {
+    df <- WebGestaltR(
         enrichMethod="ORA",
         organism="hsapiens",
-        enrichDatabase="phenotype_Human_Phenotype_Ontology",
+        enrichDatabase=database,
         interestGeneFile=interestGenes,
         interestGeneType="ensembl_gene_id",
         referenceGeneFile=referenceGenes,
@@ -25,10 +19,9 @@ run_ora <- function(interestGenes, referenceGenes) {
         isOutput=FALSE,
         setCoverNum=10,
     )
-
 }
 
-wsc <- function(df) {
+run_wsc <- function(df) {
 
     ids <- c(strsplit(df$userId, ';'))
     names(ids) <- c(df$description)
@@ -41,20 +34,48 @@ wsc <- function(df) {
 
 }
 
-interestGenes = "data/final/gene_list_distal_constrained.txt"
-referenceGenes = "data/final/gene_list_all.txt"
+main <- function(ref, refName, db, dbName, interestGenes, regionName) {
 
-df <- run_ora(interestGenes, referenceGenes)
+    df <- run_ora(interestGenes, ref, db)
 
-topSets = wsc(df)$topSets
-df <- df[df$description %in% topSets, ]
+    if (is.null(df)) {
+        return(NA)
+    }
 
-df <- df[order(enrichmentRatio), ]
+    topSets = run_wsc(df)$topSets
 
-write.table(
-    df, 
-    file="data/statistics/ora_test.tsv", 
-    quote=FALSE, 
-    sep="\t", 
-    row.names=FALSE
+    df <- df[df$description %in% topSets, ]
+    df <- df[order(df$enrichmentRatio), ]
+
+    outFile = paste0("data/statistics/ora_", regionName, "_vs_", refName, "_", dbName, ".tsv")
+
+    write.table(
+        df, 
+        file=outFile, 
+        quote=FALSE, 
+        sep="\t", 
+        row.names=FALSE,
+    )
+}
+
+referenceFiles = c(
+    rep(c("data/final/gene_list_all.txt"), 3),
+    rep(c("data/final/gene_list_gnomad_constrained.txt"), 3)
 )
+referenceNames = c(
+    rep(c("all"), 3), 
+    rep(c("gnomad"), 3)
+)
+dbs = rep(
+    c(
+        "geneontology_Biological_Process_noRedundant",
+        "geneontology_Molecular_Function_noRedundant",
+        "phenotype_Human_Phenotype_Ontology"
+    ),
+    2
+)
+dbNames = rep(c("bp","mf","hpo"), 2)
+
+args = commandArgs(trailingOnly = TRUE)
+
+mapply(main, referenceFiles, referenceNames, dbs, dbNames, args[1], args[2])
