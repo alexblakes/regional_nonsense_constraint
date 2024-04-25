e139a134c9d7af314d9d18534212aefe68fdbea0
Alex <alex.blakes@gmail.com>
2024-04-25 16:28:14 +0100

    Refactor Makefile

--- data/manual/README.md
+++
@@ -1,7 +0,0 @@
-# Data description
-Description of the contents of data/manual/.
-These files were created or edited manually. 
-They are under version control.
-
-## clinvar_vcf_header_lines.txt
-Header lines for annotating ClinVar VCF file.
--- src/clinvar/annotate_vcf.sh
+++ src/clinvar/annotate_vcf.sh
@@ -3,12 +3,17 @@
 
 VCF="data/interim/clinvar_variants_selected.vcf"
 ANNOTS="data/interim/clinvar_variants_selected.tsv"
-HEADER="data/manual/clinvar_vcf_header_lines.txt"
+HEADER="data/manual/header_lines_clinvar_vcf.txt"
 FILE_OUT="data/interim/clinvar_variants_annotated.vcf"
 
 # Index annotation file
 bgzip --keep --force $ANNOTS
 tabix --sequence 1 --begin 2 --end 2 --force "${ANNOTS}.gz"
+
+# Write header file
+echo '##INFO=<ID=ACMG,Number=1,Type=String,Description="ACMG classification">' > $HEADER
+echo '##INFO=<ID=REVIEW,Number=1,Type=String,Description="ClinVar review status">' >> $HEADER
+echo '##INFO=<ID=CV_SYMBOL,Number=1,Type=String,Description="HGNC gene symbol from ClinVar">' >> $HEADER
 
 # Annotate the VCF with ACMG and review tags
 bcftools annotate \
