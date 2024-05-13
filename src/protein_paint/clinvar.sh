96c4b0cc1fce0a82e7278bd39efcfebbf3562d2d
Alex <alex.blakes@gmail.com>
2024-05-13 15:19:08 +0100

    ClinVar variants for manual entry

--- src/protein_paint/clinvar.sh
+++ src/protein_paint/clinvar.sh
@@ -11,7 +11,6 @@ FASTA="data/raw/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
 FILE_TMP="data/interim/clinvar_vep_protein_paint_${GENE}.vcf"
 FILE_OUT_VUS="data/final/protein_paint/${GENE}_clinvar_vus.txt"
 FILE_OUT_PLP="data/final/protein_paint/${GENE}_clinvar_plp.txt"
-HEADER="gene\trefseq\tchromosome\tstart\taachange\tclass"
 
 bgzip -kf $CLINVAR
 tabix -f "${CLINVAR}.gz"
@@ -26,7 +25,6 @@ bcftools view \
     --fasta $FASTA \
     --offline \
     --cache \
-    --merged \
     --dir_cache /mnt/bmh01-rds/Ellingford_gene/.vep \
     --fork 8 \
     --buffer_size 50000 \
@@ -38,29 +36,30 @@ bcftools view \
     --symbol \
     --canonical \
     --hgvs \
-    --fields "SOURCE,Consequence,Feature,SYMBOL,CANONICAL,HGVSp,HGVSc" \
+    --fields "Consequence,Feature,SYMBOL,CANONICAL,HGVSp,HGVSc,Protein_position" \
     --output_file STDOUT \
 | filter_vep \
-    --filter "SOURCE is RefSeq" \
     --filter "SYMBOL is $GENE" \
     --filter "CANONICAL is YES" \
     --filter "Consequence in stop_gained,frameshift_variant" \
     --only_matched \
 | bcftools +split-vep \
     --columns - \
-    --format '%CHROM\t%POS\t%REF\t%ALT\t%ACMG\t%Consequence\t%Feature\t%SYMBOL\t%HGVSp' \
+    --format '%HGVSc\t%Protein_position\t%Consequence\t%ACMG' \
 | awk -v OFS="\t" \
     ' \
         { \
-            sub(/\.[0-9]+/, "", $7); \
-            sub("frameshift_variant", "frameshift", $6); \
-            sub("stop_gained", "nonsense", $6); \
-            print($8, $7, $1, $2, $NF, $6, $5) \
+            sub(/.*:c\./, "", $1) \
+            sub(/-[0-9]+/, "", $2) \
+            sub("frameshift_variant", "F", $3) \
+            sub("stop_gained", "N", $3); \
+            $1=$1; print $0 \
         } \
     ' \
 > $FILE_TMP
 
-awk -v OFS="\t" -v H=$HEADER 'BEGIN {print H}; $NF == "VUS" {NF-=1; print $0}' $FILE_TMP > $FILE_OUT_VUS
+awk -v OFS="\t" '$NF == "VUS" {NF-=1; print $0}' $FILE_TMP > $FILE_OUT_VUS
+awk -v OFS="\t" '$NF ~ /P|LP/ {NF-=1; print $0}' $FILE_TMP > $FILE_OUT_PLP
 
 # Clean up
 rm $FILE_TMP
---
+++ src/protein_paint/gnomad.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+GENE=$1
+CHR=$2
+START=$3
+END=$4
+
+CLINVAR="data/interim/clinvar_variants_annotated.vcf"
+FASTA="data/raw/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna"
+FILE_TMP="data/interim/clinvar_vep_protein_paint_${GENE}.vcf"
+FILE_OUT_VUS="data/final/protein_paint/${GENE}_clinvar_vus.txt"
+FILE_OUT_PLP="data/final/protein_paint/${GENE}_clinvar_plp.txt"
+
+bgzip -kf $CLINVAR
+tabix -f "${CLINVAR}.gz"
+
+bcftools view \
+    -r "${CHR}:${START}-${END}" "${CLINVAR}.gz" \
+    -O v \
+    -o - \
+|vep \
+    --species homo_sapiens \
+    --assembly GRCh38 \
+    --fasta $FASTA \
+    --offline \
+    --cache \
+    --dir_cache /mnt/bmh01-rds/Ellingford_gene/.vep \
+    --fork 8 \
+    --buffer_size 50000 \
+    --force_overwrite \
+    --format vcf \
+    --vcf \
+    --no_stats \
+    --minimal \
+    --symbol \
+    --canonical \
+    --hgvs \
+    --fields "Consequence,Feature,SYMBOL,CANONICAL,HGVSp,HGVSc,Protein_position" \
+    --output_file STDOUT \
+| filter_vep \
+    --filter "SYMBOL is $GENE" \
+    --filter "CANONICAL is YES" \
+    --filter "Consequence in stop_gained,frameshift_variant" \
+    --only_matched \
+| bcftools +split-vep \
+    --columns - \
+    --format '%HGVSc\t%Protein_position\t%Consequence\t%ACMG' \
+| awk -v OFS="\t" \
+    ' \
+        { \
+            sub(/.*:c\./, "", $1) \
+            sub(/-[0-9]+/, "", $2) \
+            sub("frameshift_variant", "F", $3) \
+            sub("stop_gained", "N", $3); \
+            $1=$1; print $0 \
+        } \
+    ' \
+> $FILE_TMP
+
+awk -v OFS="\t" '$NF == "VUS" {NF-=1; print $0}' $FILE_TMP > $FILE_OUT_VUS
+awk -v OFS="\t" '$NF ~ /P|LP/ {NF-=1; print $0}' $FILE_TMP > $FILE_OUT_PLP
+
+# Clean up
+rm $FILE_TMP
