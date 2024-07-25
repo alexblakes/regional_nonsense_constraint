b8b1433f500131332f84d2197094c07faab5f05b
Alex <alex.blakes@gmail.com>
2024-07-25 14:12:22 +0100

    Move directory

---
+++ src/statistics_for_plots/regions_cds_proportions.py
@@ -0,0 +1,64 @@
+"""Get the proportion of the CDS occupied by each NMD region."""
+
+import logging
+from pathlib import Path
+
+import pandas as pd
+
+import src
+from src import utils
+
+_LOGFILE = f"data/logs/{Path(__file__).stem}.log"
+_FILE_IN = "data/final/nmd_annotations_simple.tsv.gz"
+_FILE_OUT = "data/statistics/regions_cds_proportions.tsv"
+
+logger = logging.getLogger(__name__)
+
+
+def read_nmd_regions(path):
+    logger.info("Reading NMD annotation.")
+
+    return pd.read_csv(
+        path,
+        sep="\t",
+        header=None,
+        names=["chr", "pos", "enst", "region"],
+        usecols=["region"],
+        dtype="category",
+    ).loc[:, "region"]
+
+
+def proportion_value_counts(series):
+    return series.value_counts(normalize=True).rename("proportion")
+
+
+def append_full_cds(series):
+    """Append the proportion of sites in the full CDS (1)."""
+    cds = pd.Series(data=[1.0], index=["full_cds"], name="proportion")
+    return pd.concat([series, cds])
+
+
+def write_out(series, path):
+    series.to_csv(path, sep="\t")
+    return series
+
+
+def main():
+    """Run as script."""
+
+    cds_proportions = (
+        read_nmd_regions(_FILE_IN)
+        .pipe(proportion_value_counts)
+        .pipe(append_full_cds)
+        .pipe(utils.sort_index)
+        .pipe(write_out, _FILE_OUT)
+    )
+    logger.info(f"CDS proportions:\n{cds_proportions}")
+    logger.info("Done.")
+
+    return cds_proportions
+
+
+if __name__ == "__main__":
+    logger = src.setup_logger(_LOGFILE)
+    main()
