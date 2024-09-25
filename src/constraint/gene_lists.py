129a430cfcfb22038cedb51dc89971dcb718d7d8
Alex <alex.blakes@gmail.com>
2024-09-25 13:14:15 +0100

    Get constrained transcripts lists

--- src/constraint/gene_lists.py
+++ src/constraint/gene_lists.py
@@ -1,125 +1,103 @@
 """Get constrained gene lists."""
 
 import logging
-from pathlib import Path
 
 import pandas as pd
 
 import src
 from src.constraint import get_fdrs_and_gnomad_constraint
 
-_LOGFILE = f"data/logs/{Path(__file__).stem}.log"
 _REGIONAL_NONSENSE_CONSTRAINT = "data/final/regional_nonsense_constraint.tsv"
 _GENE_IDS = "data/interim/gene_ids.tsv"
 _GNOMAD_V4_CONSTRAINT = "data/raw/gnomad.v4.0.constraint_metrics.tsv"
-_FILE_OUT_ALL = "data/final/gene_list_all.txt"
-_FILE_OUT_GNOMAD_CST = "data/final/gene_list_gnomad_constrained.txt"
-_FILE_OUT_NMD_TARGET = "data/final/gene_list_nmd_target_constrained.txt"
-_FILE_OUT_START_PROX = "data/final/gene_list_start_proximal_constrained.txt"
-_FILE_OUT_LONG_EXON = "data/final/gene_list_long_exon_constrained.txt"
-_FILE_OUT_DISTAL = "data/final/gene_list_distal_constrained.txt"
+_FILE_OUT_CANONICAL = "data/final/transcript_list_all.txt"
 
 logger = logging.getLogger(__name__)
 
 
-def read_gene_ids(path=_GENE_IDS):
-    return pd.read_csv(path, sep="\t", usecols=["gene_id", "transcript_id"]).set_axis(
-        ["ensg", "enst"], axis="columns"
+def get_transcript_ids(path=_GENE_IDS):
+    return pd.read_csv(path, sep="\t").loc[:, "transcript_id"].rename("enst")
+
+
+def get_constrained_gnomad_transcripts(df):
+    m1 = df.pli > 0.9
+    m2 = df.loeuf < 0.6
+
+    constrained_transcripts = df[m1 | m2]["enst"].drop_duplicates()
+
+    return constrained_transcripts
+
+
+def read_regional_nonsense_constraint(path=_REGIONAL_NONSENSE_CONSTRAINT):
+    return pd.read_csv(
+        path,
+        sep="\t",
+        usecols=["enst", "region", "constraint"],
     )
 
 
-def get_constrained_gnomad_transcripts(gnomad):
-    logger.info("Getting constrained gnomAD transcripts.")
+def get_constrained_regions(df, region):
+    canonical_transcripts = get_transcript_ids()
 
-    m1 = gnomad.pli > 0.9
-    m2 = gnomad.loeuf < 0.6
-
-    gnomad_strong = gnomad[m1 | m2]["enst"].drop_duplicates()
-
-    logger.info(f"Constrained transcripts in gnomAD: {len(gnomad_strong)}")
-
-    return gnomad_strong
-
-
-#! The merge causes a handful of transcripts to be lost. Try to refactor without 
-#! needing to merge with gene IDs.
-def get_gene_ids(df, gene_ids):
-    """Get gene IDs for a dataframe with a list of transcript IDs."""
-
-    df = gene_ids.merge(df, how="inner", on="enst", validate="one_to_one")["ensg"]
-    assert df.duplicated().sum() == 0, "Duplicated gene IDs."
-
-    logger.info(f"Gene ids: {len(df)}")
-
-    return df
-
-
-def get_regional_constrained_genes(
-    path=_REGIONAL_NONSENSE_CONSTRAINT, region="nmd_target"
-):
-    logger.info(region)
-
-    rnc = (
-        pd.read_csv(
-            path,
-            sep="\t",
-            usecols=["enst", "region", "constraint"],
-        )
-        .query("region == @region")
+    constrained_transcripts = (
+        df.loc[lambda x: x["region"].isin(region)]
         .query('constraint == "constrained"')
+        .loc[lambda x: x.enst.isin(canonical_transcripts), "enst"]
+        .drop_duplicates()
     )
 
-    assert (
-        rnc.enst.duplicated().sum() == 0
-    ), f"Duplicated transcript IDs in {region} region."
+    logger.info(
+        f'Constrained transcripts in "{region}" region: {len(constrained_transcripts)}'
+    )
 
-    assert len(rnc) > 0, "Empty dataframe."
+    return constrained_transcripts
 
-    return rnc
+
+def write_out(series, path):
+    series.to_csv(path, sep="\t", index=False)
+    return series
 
 
 def main():
     """Run as script."""
 
-    # Read data
-    gene_ids = read_gene_ids(_GENE_IDS)
-    gnomad = get_fdrs_and_gnomad_constraint.get_gnomad_constraint(_GNOMAD_V4_CONSTRAINT)
+    canonical_transcripts = get_transcript_ids()
 
-    # Define gene lists
-    ## All genes with a protein-coding canonical transcript
-    _all = gene_ids["ensg"].drop_duplicates()
-    logger.info(f"All genes: {len(_all)}")
-
-    ## Constrained genes in gnomAD
-    gnomad_strong = get_constrained_gnomad_transcripts(gnomad).pipe(
-        get_gene_ids, gene_ids
+    gnomad = (
+        get_fdrs_and_gnomad_constraint.get_gnomad_constraint(_GNOMAD_V4_CONSTRAINT)
+        .pipe(get_constrained_gnomad_transcripts)
+        .loc[lambda x: x.isin(canonical_transcripts)]
     )
+    logger.info(f"Constrained transcripts in gnomAD: {len(gnomad)}")
 
-    ## Genes with regional nonsense constraint
-    nmd_target = get_regional_constrained_genes().pipe(get_gene_ids, gene_ids)
-    start_proximal = get_regional_constrained_genes(region="start_proximal").pipe(
-        get_gene_ids, gene_ids
+    regional_constraint = read_regional_nonsense_constraint()
+    
+    any_region = get_constrained_regions(
+        regional_constraint, ["nmd_target", "start_proximal", "long_exon", "distal_nmd"]
     )
-    long_exon = get_regional_constrained_genes(region="long_exon").pipe(
-        get_gene_ids, gene_ids
-    )
-    distal = get_regional_constrained_genes(region="distal_nmd").pipe(
-        get_gene_ids, gene_ids
-    )
+    nmd_target = get_constrained_regions(regional_constraint, ["nmd_target"])
+    start_proximal = get_constrained_regions(regional_constraint, ["start_proximal"])
+    long_exon = get_constrained_regions(regional_constraint, ["long_exon"])
+    distal = get_constrained_regions(regional_constraint, ["distal_nmd"])
 
-    # Write to output
-    write_out = lambda x, y: x.to_csv(y, index=False, header=None)
+    write_out(canonical_transcripts, _FILE_OUT_CANONICAL)
 
-    write_out(_all, _FILE_OUT_ALL)
-    write_out(gnomad_strong, _FILE_OUT_GNOMAD_CST)
-    write_out(nmd_target, _FILE_OUT_NMD_TARGET)
-    write_out(start_proximal, _FILE_OUT_START_PROX)
-    write_out(long_exon, _FILE_OUT_LONG_EXON)
-    write_out(distal, _FILE_OUT_DISTAL)
+    transcripts_dict = {
+        "gnomad": gnomad,
+        "any_region": any_region,
+        "nmd_target": nmd_target,
+        "start_proximal": start_proximal,
+        "long_exon": long_exon,
+        "distal": distal,
+    }
+
+    for name, transcripts in transcripts_dict.items():
+        path = f"data/final/transcript_list_{name}_constrained.txt"
+        write_out(transcripts, path)
 
     return None
 
 
 if __name__ == "__main__":
-    logger = src.setup_logger(_LOGFILE)
+    logger = src.setup_logger(__file__)
     main()
